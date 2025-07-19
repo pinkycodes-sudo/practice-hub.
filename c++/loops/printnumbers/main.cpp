c5b6ecb144cd2ac385841371d954f5ffc9cf314d
pinkycodes-sudo <p6529797@gmail.com>
2025-07-20 01:30:33 +0200

    feat: implement for loop to print numbers from 0 to 9

---
+++ c++/loops/printnumbers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //loop from 0 to 9 , which is 10 numbers in total.
+    for(int i = 0; i < 10; i++)
+
+    //print the current value of i followed by space.
+    cout << i << " ";
+    
+    //print a new line after the loop finishes.
+    cout << endl;
+
+       return 0;
+}
