3a6b01071f79b0ddce1496ba991c24cc80bff3f6
pinkycodes-sudo <p6529797@gmail.com>
2025-07-20 01:33:58 +0200

    feat: implement countdown from 10 to 0 using for loop

---
+++ c++/loops/countbackwards/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // loop from 10 to 0.
+    for(int i = 10; i >= 0; i--)
+
+    // print the current value of i followed by space.
+    cout << i << " ";
+
+    //print a new line after the loop finishes.
+
+    cout << endl;
+
+    return 0;
+
+}
