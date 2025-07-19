11021470cb478447067bf164c807b70d40a34106
pinkycodes-sudo <p6529797@gmail.com>
2025-07-20 01:37:41 +0200

    feat: output powers of 5 up to 500000 using for loop

---
+++ c++/loops/powersof5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // loop from 5 keep multiplying by 5 until less than 500000.
+    for(int i = 5; i < 500000; i = 5 * i)
+
+    // print the current power of 5 followed by space.
+    cout << i << " ";
+
+    // print a newline after the loop finishes.
+    cout << endl;
+
+    return 0;
+
+
+}
