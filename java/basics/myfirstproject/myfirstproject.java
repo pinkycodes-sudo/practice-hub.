450e95d5b7f4f80b6ba2bbf0ede1360f73fc9b2f
pinkycodes-sudo <p6529797@gmail.com>
2025-08-01 19:17:59 +0200

    Add my first java project

---
+++ java/basics/myfirstproject/myfirstproject.java
@@ -0,0 +1,11 @@
+public class Myfirstproject {
+
+    public static void main(String[] args) {
+        System.out.print("I love pizza!\n");
+        System.out.println("It's so good.");
+        System.out.print("I could eat it everyday.\n");
+        System.out.print("Even when coding.");
+        
+        
+    }
+}
